4effbebd2dc783d24113ccf34a322dda3f4905f7
shivam <shivam.gosavi340@gmail.com>
2015-09-12 10:49:47 +0530

    added model classes

---
+++ app/src/main/java/com/learn/model/LoginRequest.java
@@ -0,0 +1,57 @@
+package com.learn.model;
+
+import com.google.gson.annotations.Expose;
+
+/**
+ * Created by chetan on 15/05/15.
+ */
+public class LoginRequest {
+    @Expose
+    private String LoginID;
+    @Expose
+    private String Password;
+
+    public LoginRequest(String loginID, String password) {
+        LoginID = loginID;
+        Password = password;
+    }
+
+    /**
+     * @return The LoginID
+     */
+    public String getLoginID() {
+        return LoginID;
+    }
+
+    /**
+     * @param LoginID The LoginID
+     */
+    public void setLoginID(String LoginID) {
+        this.LoginID = LoginID;
+    }
+
+    public LoginRequest withLoginID(String LoginID) {
+        this.LoginID = LoginID;
+        return this;
+    }
+
+    /**
+     * @return The Password
+     */
+    public String getPassword() {
+        return Password;
+    }
+
+    /**
+     * @param Password The Password
+     */
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public LoginRequest withPassword(String Password) {
+        this.Password = Password;
+        return this;
+    }
+
+}
