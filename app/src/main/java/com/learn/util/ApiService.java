75660e8472106bba76882ce80595f30a9c326929
shivam <shivam.gosavi340@gmail.com>
2015-09-12 10:52:52 +0530

    added login api call

--- app/src/main/java/com/learn/util/ApiService.java
+++ app/src/main/java/com/learn/util/ApiService.java
@@ -1,10 +1,17 @@
 package com.learn.util;
 
+import com.learn.model.LoggedInUser;
+import com.learn.model.LoginRequest;
+
+import retrofit.Call;
+import retrofit.http.Body;
+import retrofit.http.POST;
+
 /**
  * Created by shivam on 12/9/15.
  */
 public interface ApiService {
 
-
-
+    @POST("/Login")
+    Call<LoggedInUser> login(@Body LoginRequest loginRequest);
 }
